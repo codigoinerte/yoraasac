ea04ef2470b9cfeb5d7c2853299fda15962bafb7
Fredy Martinez Bustamante <125488003+FreddyMartinezBabilonia@users.noreply.github.com>
2023-05-06 14:38:53 -0500

    update - store/store.tsx - productos y helados añadidos al script

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,13 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { authSlice, personasSlice } from './';
+import { authSlice, personasSlice, stockBarquillosSlice, stockBateriasSlice, stockHeladosSlice } from './';
 import { generalSlice } from './personas/generalSlice';
+import { productosSlice } from './productos/productosSlice';
 // uiSlice, calendarSlice, 
 
 export const store = configureStore({
     reducer: {
         auth:     authSlice.reducer,
         personas: personasSlice.reducer,
-        general: generalSlice.reducer
+        general: generalSlice.reducer,
+        productos: productosSlice.reducer,
+        stockHelados: stockHeladosSlice.reducer,
+        stockBaterias: stockBateriasSlice.reducer,
+        stockBarquillos: stockBarquillosSlice.reducer,
+        
         // calendar: calendarSlice.reducer,
         // ui:       uiSlice.reducer
     },
