98f88d6f5e0c8fc589f80b1a2a0b40bf81144fbe
Fredy Martinez Bustamante <125488003+FreddyMartinezBabilonia@users.noreply.github.com>
2023-05-06 14:24:41 -0500

    new - useStockBarquillosStore

---
+++ src/hooks/useStockBarquillosStore.tsx
@@ -0,0 +1,155 @@
+import axios from 'axios';
+import React, { useReducer } from 'react'
+import { toast } from 'react-hot-toast';
+import { useSelector, useDispatch } from 'react-redux';
+import { backendApi } from '../api';
+import { toastMessage } from '../helpers';
+import { IRootState } from '../interfaces';
+import { FormBuscarStockBarquilloValues, FormStockBarquilloValues } from '../panel/interfaces';
+import { onStatus, onStockBarquilloList, onSetStockBarquilloActive, onStockBarquilloDelete, onStockBarquilloAddMessage, onStockBarquilloClearMessage } from '../store'
+import { useHelpers } from './useHelpers';
+
+
+export const useStockBarquillosStore = () => {
+  
+    const rutaEndpoint = '/stock-bateria';
+
+    const { StockBarquillo, active, nextPage, prevPage } = useSelector((state:IRootState)=>state.stockBarquillos);
+
+    const { status, errorMessage } = useSelector((state:IRootState)=>state.general)
+
+    const dispatch = useDispatch();
+
+    const loadStockBarquillo = async (pagina = "1", buscar:FormBuscarStockBarquilloValues) =>{
+
+        dispatch(onStatus(true));
+        
+        try {
+            
+            const {codigo, movimiento, fechaCreacion } = buscar;            
+            const { data } = await backendApi.get(rutaEndpoint, {
+                params:{
+                    page: pagina,
+                    codigo,
+                    movimiento,
+                    fecha: fechaCreacion
+                }
+            });
+            
+            dispatch(onStatus(false));
+            dispatch(onStockBarquilloList(data));            
+
+        } catch (error) {
+            
+            if (axios.isAxiosError(error)) {
+
+                const { message } = error.response?.data;               
+
+                dispatch( onStockBarquilloAddMessage(message) );
+                setTimeout(() => {
+                    dispatch( onStockBarquilloClearMessage() );
+                }, 10);
+
+                dispatch(onStatus(false));
+
+                return error.message;                
+
+            } else {
+                console.log('unexpected error: ', error);
+                return 'An unexpected error occurred';
+            }
+        }
+    }
+
+    const saveStockBarquillo = async ( postdata:FormStockBarquilloValues) => {
+        dispatch(onStatus(true));
+
+        try {
+
+            const { data:info } = await backendApi.post(rutaEndpoint, postdata);
+            const result = info.data;
+            
+            toastMessage(info);
+            
+            dispatch(onSetStockBarquilloActive({
+                ...result               
+            }));
+
+            
+            dispatch(onStatus(false));
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+    const updateStockBarquillo = async (postdata:FormStockBarquilloValues) => {
+        dispatch(onStatus(true));
+
+        try {
+
+            const { data:info } = await backendApi.put(`${rutaEndpoint}/${active!.id}`, postdata);
+            const result = info.data;
+            
+            toastMessage(info);
+            
+            dispatch(onSetStockBarquilloActive({
+                ...result                
+            }));
+            dispatch(onStatus(false));
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const getStockBarquillo = async (id:number) => {
+        
+        dispatch(onStatus(true));
+
+        try {            
+
+            const { data:info } = await backendApi.get(`${rutaEndpoint}/${id}`);
+            
+            dispatch(onSetStockBarquilloActive(info.data));
+
+            dispatch(onStatus(false));
+
+            return info.data;
+
+        } catch (error) {
+            return null;
+         }
+    }
+
+    const deleteStockBarquillo = async (id:number):Promise<Boolean> =>{
+        
+        try {            
+
+            const { data:info } = await backendApi.delete(`${rutaEndpoint}/${id}`);
+                        
+            dispatch(onStockBarquilloDelete(info.data.id));
+
+            dispatch(onStatus(false));
+            return true;            
+        } catch (error) {
+            // console.log(error);
+            return false;
+        }
+    }    
+
+    return {
+        status, 
+        StockBarquillo, 
+        active, 
+        errorMessage,
+        nextPage,
+        prevPage,
+
+        loadStockBarquillo,
+        saveStockBarquillo,
+        updateStockBarquillo,
+        getStockBarquillo,
+        deleteStockBarquillo,
+    }
+}
