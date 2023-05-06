1187438e036cfb68746df73d8629ee21391c2adc
Fredy Martinez Bustamante <125488003+FreddyMartinezBabilonia@users.noreply.github.com>
2023-05-06 14:19:44 -0500

    se agrego estilos de rsuite en app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './router';
 import { store } from './store/store';
+import 'rsuite/styles/index.less'; // or 'rsuite/dist/rsuite.min.css'
 
 export const App = () => {
   return (
