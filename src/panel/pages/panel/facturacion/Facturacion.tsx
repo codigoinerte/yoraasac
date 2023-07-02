3d4b0fc2aba4525bea32a558b4b22bc727cab8c6
FreddyMartinezBabilonia <fredy.martinez@babilonia.io>
2023-07-02 15:52:15 -0500

    se completo el script de listado de facturacion

--- src/panel/pages/panel/facturacion/Facturacion.tsx
+++ src/panel/pages/panel/facturacion/Facturacion.tsx
@@ -1,8 +1,12 @@
 import queryString from 'query-string';
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 import { ContainerInner, List } from '../../../components'
-import { breadcrumb as bread, listaDetalle, paginationInterface } from '../../../interfaces';
+import { BuscarFacturas, FormBuscarFacturasValues, breadcrumb as bread, listaDetalle, paginationInterface } from '../../../interfaces';
+import { useAlert, useFacturastore, useHelpers } from '../../../../hooks';
+import Swal from 'sweetalert2';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { CurrencyConvert } from '../../../helpers/CurrencyConvert';
 
 const breadcrumb:bread[] = [
     { id:1, titulo: 'Facturación', enlace: '' },
@@ -11,7 +15,7 @@ const breadcrumb:bread[] = [
   
 export const Facturacion = () => {
 
-
+/*
     const cabecera = [
         "num. doc.",
         "Documento",        
@@ -54,6 +58,170 @@ export const Facturacion = () => {
     const location = useLocation();
   
     const { q = '' } = queryString.parse(location.search);
+*/
+
+
+    const buttonPrev = useRef<any>();
+    const buttonNext = useRef<any>();
+
+    const [pageActive, setPageActive] = useState<string | null>("1");
+
+    const [buscar, setBuscar] = useState<BuscarFacturas>({
+        documento: null,
+        nombre:null,
+        fecha: null,
+        buscar:false
+    });
+ 
+    const cabecera = [
+        "num. doc.",
+        "Documento",        
+        "Cliente",
+        "Moneda",
+        "Monto",
+        "Estado",
+        "Fecha de creación"
+    ];
+
+    const { warningDelete } = useAlert();
+
+
+    const { listEstadoHeladero, listNotaHeladeroEstado} = useHelpers();
+
+    const { status, facturas, nextPage, prevPage, loadFacturacion, deleteFacturacion } = useFacturastore();
+
+    useEffect(() => {
+    
+        loadFacturacion(pageActive!=null ? pageActive : "1", buscar);        
+        
+    }, [pageActive, buscar]);
+
+    useEffect(() => {
+        listNotaHeladeroEstado();        
+    }, [])
+
+
+    const detalle:listaDetalle[] = facturas.map(({ id,serie, correlativo, usuario_nombre, created_at, estado, documento, moneda, total}) => {
+
+        let monto = '';
+        if(!isNaN(parseFloat(total)))
+        {
+            monto = new Intl.NumberFormat('es-PE', 
+            {
+                style: 'currency',
+                currency: 'PEN'
+            })
+            .format(parseFloat(total));
+        }
+
+        return {
+            id: id.toString(),
+            campos: [    
+                `${serie}-${correlativo}`,
+                documento,
+                usuario_nombre??''.toString(),
+                moneda??''.toString(),
+                monto??''.toString(),
+                estado??''.toString(),
+                (created_at??'').toString()
+            ]
+    
+        }
+    });
+
+    const eliminar = (id:number) => {
+        warningDelete(async function(){
+            
+        const result = await deleteFacturacion(id);                
+
+        if(result){
+            Swal.fire(
+                'Eliminado!',
+                'Tu registro fue eliminado con exito.',
+                'success'
+            )
+        }else{
+            Swal.fire(
+                'Error',
+                'Hubo un error al momento de ejecutar el proceso vuelva a intentarlo mas tarde',
+                'question'
+                )
+        }
+
+    
+        });
+    }
+
+    const next = (e:paginationInterface) => {
+
+        buttonNext.current = e.target;
+
+        if(nextPage == null) 
+        {
+            // e.currentTarget.setAttribute("disabled", "true");
+        }
+        else
+        {
+            // e.currentTarget.removeAttribute("disabled");
+            setPageActive(nextPage);
+        }
+
+        if(prevPage == null)
+        {
+            // buttonPrev.current?.removeAttribute("disabled");
+        }                 
+    }
+
+    const prev = (e:paginationInterface) => {
+        
+        buttonPrev.current = e.target;
+
+        if(prevPage == null) 
+        {
+            // e.currentTarget.setAttribute("disabled", "true");
+        }
+        else
+        {
+            // e.currentTarget.removeAttribute("disabled");
+            setPageActive(prevPage);
+        }
+
+        if(nextPage == null)
+        {
+            // buttonNext.current?.removeAttribute("disabled");
+        }
+    }
+
+    const { register, handleSubmit, reset, formState:{ errors } } = useForm<FormBuscarFacturasValues>();
+
+    const onSubmit: SubmitHandler<FormBuscarFacturasValues> = ({ documento, nombre, fecha }) => {
+
+        if(documento != '' || nombre!= '' || fecha?.toString() != '') 
+        {
+            setPageActive(null);
+            setBuscar({documento, nombre, fecha, buscar:true})
+
+        }
+        else
+        {
+            false
+            // agregar toast booostrap in version react, buscar alternativa solo codigo
+
+        }
+    };
+
+    const resetQuery = () => {
+    
+        reset({
+            documento:null,
+            nombre:null,
+            fecha:null
+        });
+
+        const buscar = {documento:null, nombre:null, fecha:null, buscar:true};
+        
+        loadFacturacion("1", buscar);
+    }
 
     return (
         <ContainerInner breadcrumb={breadcrumb}>
@@ -66,7 +234,7 @@ export const Facturacion = () => {
                     next={next}
                     prev={prev}>
                 <>
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="row">
                         <div className="col-xs-12">
                             <h5>Buscador</h5>
@@ -76,25 +244,26 @@ export const Facturacion = () => {
 
                         <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                             <div className="mb-3">
-                                <label htmlFor="documento" className="form-label">Documento</label>
-                                <input type="text" className="form-control" id="documento" aria-describedby="Buscador" placeholder='Documento'/>
+                                <label htmlFor="documento" className="form-label">Num doc.</label>
+                                <input type="text" className="form-control" id="documento" aria-describedby="Buscador" placeholder='Documento' {...register('documento')}/>
                             </div>
                         </div>
                         <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4">
                             <div className="mb-3">
                                 <label htmlFor="nombres" className="form-label">Nombre y apellido</label>
-                                <input type="text" className="form-control" id="nombres" aria-describedby="Buscador" placeholder='Nombre y/o apellido'/>
+                                <input type="text" className="form-control" id="nombres" aria-describedby="Buscador" placeholder='Nombre y/o apellido' {...register('nombre')}/>
                             </div>
                         </div>
                         <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
                             <div className="mb-3">
                                 <label htmlFor="fecha_creacion" className="form-label">Fecha de creaci&oacute;n</label>
-                                <input type="date" placeholder="dd-mm-yyyy" className="form-control" id="fecha_creacion" aria-describedby="fechacreacion"/>
+                                <input type="date" placeholder="dd-mm-yyyy" className="form-control" id="fecha_creacion" aria-describedby="fechacreacion" {...register('fecha')}/>
                             </div>
                         </div>
                         <div className="col-xs-12 col-sm-6 col-md-4 col-lg-2 d-flex align-items-end">
                             <div className="mb-3 w-100">                                
                                 <button className="btn btn-primary text-center w-100" type="submit"><i className="bi bi-search"></i> Buscar</button>
+                                <button onClick={resetQuery} className="btn btn-secondary text-center w-100 mt-1"><i className="bi bi-x-lg"></i> Resetear</button>
                             </div>
                         </div>
 
