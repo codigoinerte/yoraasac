9c9d4890ed577213f2f0492f604ee327f594641c
codigoinerte <fmartinez.bpe@gmail.com>
2024-06-09 23:41:54 -0500

    fix: correccion de uso de montos con string a number, correccion de suma de subtotal y decimales

--- src/panel/pages/panel/notaHeladero/NotaHeladeroDetalle.tsx
+++ src/panel/pages/panel/notaHeladero/NotaHeladeroDetalle.tsx
@@ -35,7 +35,7 @@ export const NotaHeladeroDetalle = () => {
 
     const [orderDirection, setOrderDirection] = useState<order>("asc");
 
-    const [estadoTitulo, setEstadoTitulo] = useState('Nueva cuenta');
+    const [estadoTitulo, setEstadoTitulo] = useState('Apertura de cuenta nueva');
 
     const [isNewRegister, setisNewRegister] = useState(false);
 
@@ -77,7 +77,7 @@ export const NotaHeladeroDetalle = () => {
         let vendido = (!getValues(`productos.${index}.vendido`)) ? 0 : parseFloat(getValues(`productos.${index}.vendido`)!.toString());
         let precio_operacion = (!getValues(`productos.${index}.precio_operacion`)) ? 0 : parseFloat(getValues(`productos.${index}.precio_operacion`)!.toString());
         let precio_final = precio_operacion * parseInt(vendido.toString());        
-        setValue(`productos.${index}.importe`, precio_final.toString());
+        setValue(`productos.${index}.importe`, precio_final);
     }
     const calculo_precio_final = (precio_venta:string, descuento:string) => {
         let p = precio_venta==""?0: parseFloat(precio_venta);
@@ -90,11 +90,10 @@ export const NotaHeladeroDetalle = () => {
     }
 
     const onSubmit: SubmitHandler<FormNotaHeladeroValues> = (data) => {
-        
-        if(refId.current == 0){            
+        if(refId.current == 0){
             saveNotaHeladero({...data});
             setisNewRegister(false);
-
+            refId.current = active?.id;
         }else{            
             updateNotaHeladero({...data});
         }
@@ -151,19 +150,19 @@ export const NotaHeladeroDetalle = () => {
 
             if(heladero?.estado != undefined) {
 
-               if(heladero.estado == 3){ // si el estado es de guardado (nuevo o paso 1)
-                    setValue('estado', 2); // el estado pasa  a apertura (se busca aperturar lo que el dia anterior se guardo)
-                    setEstadoTitulo('Reabriendo cuenta guardada');
-               }
+                if(heladero.estado == 2){ // si el estado es de apertura (paso 2)
+                    setValue('estado', 2); //el estado pasa a cierre (se busca primero cerrar la cuenta del heladero)
+                    setEstadoTitulo('Reapertura');
+                }                             
                
-               if(heladero.estado == 2 && heladero.fecha_cierre == null){ // si el estado es de apertura (paso 2)
-                    setValue('estado', 1); //el estado pasa a cierre (se busca primero cerrar la cuenta del heladero)
-                    setEstadoTitulo('Cerrando cuenta del dia');
-               }
-               
-               if(heladero.estado == 2 && heladero.fecha_cierre != null){
-                    setEstadoTitulo('Cuenta cerrada');
-               }           
+                if(heladero.estado == 1){
+                    setEstadoTitulo('Apertura de cuenta nueva');
+                }
+
+                if(heladero.estado == 3){
+                    setEstadoTitulo('Cuenta guardada');
+                }
+
                setState(heladero.estado);
             }
 
@@ -192,13 +191,13 @@ export const NotaHeladeroDetalle = () => {
 
             let dateNow = moment(new Date()).format("YYYY-MM-DD HH:mm").toString();                
             setValue('fecha_operacion', dateNow.replace(" ","T"));            
-            setEstadoTitulo('Guardando productos del dia');
+            setEstadoTitulo('Apertura de cuenta nueva');
 
             fields.map((_, index) => {
                 setValue(`productos.${index}.pedido`, 0);
                 setValue(`productos.${index}.devolucion`, 0);
                 setValue(`productos.${index}.vendido`, 0);
-                setValue(`productos.${index}.importe`, '');
+                setValue(`productos.${index}.importe`, 0);
             });
         }
 
@@ -206,7 +205,7 @@ export const NotaHeladeroDetalle = () => {
 
     const redirectToFactura = () => navigate(`/facturacion/new?gf=nota&id=${refId.current}`); 
 
-    const getDisableDate = (id:number) => ((id == 1 && active?.fecha_cierre) || (id == 2 && active?.fecha_apertura) || (id == 3 && active?.fecha_guardado)) ? true : false;
+    const getDisableDate = (id:number) => ((id == 1 && active?.fecha_cierre) || (id == 2 && active?.fecha_apertura) || (id == 3 && (active?.fecha_guardado || active?.estado == 3)) || (id == 1 && active?.estado == 2 && active.fecha_cierre == null) ) ? true : false;
 
     const componentRef = useRef(null);
 
@@ -227,7 +226,7 @@ export const NotaHeladeroDetalle = () => {
         await listNotaHeladeroEstado();
     
         const listProductosPublicados = await loadProductosDisponibles();
-                            
+
         //si no hay id principal
         if(refId.current == 0)
         {
@@ -244,7 +243,7 @@ export const NotaHeladeroDetalle = () => {
                     devolucion : 0,
                     pedido : 0,
                     vendido : 0,
-                    importe : "0",
+                    importe : 0,
                     nota_heladeros_id : 0,
                     created_at : item.created_at,
                     updated_at : item.updated_at,
@@ -286,7 +285,6 @@ export const NotaHeladeroDetalle = () => {
 
                 setValue('estado', estado);
             }
-            
             setState(heladero.estado);
             
             let dateNow = moment(new Date()).format("YYYY-MM-DD HH:mm").toString();                        
@@ -305,10 +303,10 @@ export const NotaHeladeroDetalle = () => {
                 setValue('user_id', heladero?.user_id);
             }
 
-            setValue(`monto`, (heladero.monto??0).toString());
-            setValue(`pago`, (heladero.pago??0).toString());
-            setValue(`ahorro`, (heladero.ahorro??0).toString());
-            setValue(`debe`, (heladero.debe??0).toString());
+            setValue(`monto`, (heladero.monto??0));
+            setValue(`pago`, (heladero.pago??0));
+            setValue(`ahorro`, (heladero.ahorro??0));
+            setValue(`debe`, (heladero.debe??0));
         }
     }
         
@@ -367,31 +365,34 @@ export const NotaHeladeroDetalle = () => {
 
             let subtotal = 0;
             fields.forEach((item, index) => {
-                
-                const pedido = getValues(`productos.${index}.pedido`)??0;
-                const devolucion = getValues(`productos.${index}.devolucion`)??0;
-                const devolucion_today = getValues(`productos.${index}.devolucion_today`) ?? 0;
+
+                const devolucion_today_saved = (active) ? ( active.detalle.find((item) => item.codigo == getValues(`productos.${index}.codigo`))!.devolucion_today ?? 0) : 0;
+                setValue(`productos.${index}.devolucion_today`, devolucion_today_saved);
+
+                const pedido = parseInt((getValues(`productos.${index}.pedido`)??0).toString());
+                const devolucion = parseInt((getValues(`productos.${index}.devolucion`)??0).toString());
+                const devolucion_today = parseInt((getValues(`productos.${index}.devolucion_today`) ?? devolucion_today_saved).toString());
                 const precio_operacion = getValues(`productos.${index}.precio_operacion`)??0;
 
-                const vendido = ((devolucion + pedido) - devolucion_today);
-                const importe = (vendido * precio_operacion).toFixed(2);
+                const vendido = ((devolucion+pedido)-devolucion_today);
+                const importe = parseFloat((vendido * precio_operacion).toFixed(2));
 
                 setValue(`productos.${index}.vendido`, vendido);
-                setValue(`productos.${index}.importe`, importe.toString());
-                subtotal+=parseFloat(importe);
+                setValue(`productos.${index}.importe`, importe);
+                subtotal+=parseFloat(importe.toString());
             })
 
             if(subtotal < 0) {
-                setValue(`monto`, (0.00).toString());
-                setValue(`pago`, (0.00).toString());
-                setValue(`ahorro`, (0.00).toString());
-                setValue(`debe`, (0.00).toString());
+                setValue(`monto`, 0);
+                setValue(`pago`, 0);
+                setValue(`ahorro`, 0);
+                setValue(`debe`, 0);
             }
             
-            setValue(`monto`, subtotal.toString());
-            setValue(`pago`, (0.00).toString());
-            setValue(`ahorro`, (0.00).toString());
-            setValue(`debe`, subtotal.toString());           
+            setValue(`monto`, parseFloat(subtotal.toFixed(2)));
+            setValue(`pago`, 0);
+            setValue(`ahorro`, 0);
+            setValue(`debe`, parseFloat(subtotal.toFixed(2)));
             return;
         }
 
@@ -568,6 +569,7 @@ export const NotaHeladeroDetalle = () => {
                                                     <td data-tooltip-id={`tooltip-html-${index}`}
                                                         data-tooltip-html={`
                                                         <div style="text-align:left">
+                                                            <b>Codigo</b>:${getValues(`productos.${index}.codigo`)??0}<br/>
                                                             <b>Precio</b>: S/ ${getValues(`productos.${index}.precio_operacion`)??0}
                                                         </div>
                                                         `}>
@@ -610,12 +612,12 @@ export const NotaHeladeroDetalle = () => {
                                                         <td align='center'>Pago</td>
                                                         <td><input type="text" {...register('pago', {
                                                             onChange: (e) =>{
-                                                                const monto = parseFloat(getValues('monto') ?? 0);
-                                                                const ahorro = parseFloat(getValues('ahorro') ?? 0);
+                                                                const monto = (getValues('monto') ?? 0);
+                                                                const ahorro = (getValues('ahorro') ?? 0);
                                                                 const pago = parseFloat(e.target.value ?? 0);
 
                                                                 const debe = (monto-(pago+ahorro)).toFixed(2);
-                                                                setValue('debe', debe.toString());
+                                                                setValue('debe', parseFloat(debe));
                                                                 
                                                             }
                                                         })} className='form-control' /></td>
@@ -625,12 +627,12 @@ export const NotaHeladeroDetalle = () => {
                                                         <td align='center'>Ahorro</td>
                                                         <td><input type="text" {...register('ahorro', {
                                                             onChange: (e) =>{
-                                                                const monto = parseFloat(getValues('monto') ?? 0);
+                                                                const monto = (getValues('monto') ?? 0);
                                                                 const ahorro = parseFloat(e.target.value ?? 0);
-                                                                const pago = parseFloat(getValues('pago') ?? 0);
+                                                                const pago = (getValues('pago') ?? 0);
 
                                                                 const debe = (monto-(pago+ahorro)).toFixed(2);
-                                                                setValue('debe', debe.toString());
+                                                                setValue('debe', parseFloat(debe));
                                                             }
                                                         })} className='form-control' /></td>
                                                     </tr>
@@ -666,12 +668,17 @@ export const NotaHeladeroDetalle = () => {
                 
                 </form>
 
-                <ModalNotaHeladeroRegister
-                        openModal={openModal}
-                        handlerOpenModal={setOpenModal}
-                        setValueOrigin={setValue}
-                        updateStateHeladero={setState}
-                    />
+                {
+
+                    (active?.estado!=3) &&
+                    (<ModalNotaHeladeroRegister
+                            openModal={openModal}
+                            handlerOpenModal={setOpenModal}
+                            setValueOrigin={setValue}
+                            getValuesOrigin={getValues}
+                            updateStateHeladero={setState}
+                        />)
+                }
             </>
         </ContainerInner>
     )
