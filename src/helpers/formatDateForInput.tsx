d0955e5c4a92f88588a02b9b998cfcb399301398
codigoinerte <fmartinez.bpe@gmail.com>
2024-07-26 20:57:41 -0500

    fix: corrección en formato de fecha

--- src/helpers/formatDateForInput.tsx
+++ src/helpers/formatDateForInput.tsx
@@ -2,5 +2,5 @@ import moment from "moment";
 type dateparam = Date | string;
 export const formatDateForInput = (date:dateparam) => {
     const newDate = new Date(date);
-    return moment(newDate).format("yyyy-MM-DD hh:mm:ss");
+    return moment(newDate).format("yyyy-MM-DDThh:mm");
 }
