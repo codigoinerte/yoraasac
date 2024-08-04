770337bc6e672aba06a8d36e10c58c4fcc110ecb
codigoinerte <fmartinez.bpe@gmail.com>
2024-08-04 16:40:17 -0500

    fix: se modifico CurrencyConvert para que acepte decimales

--- src/panel/helpers/CurrencyConvert.tsx
+++ src/panel/helpers/CurrencyConvert.tsx
@@ -1,8 +1,10 @@
-export const CurrencyConvert =(valor:any)=>{
+export const CurrencyConvert =(valor:any, onlyformat = false)=>{
 
     let number = parseFloat(valor);
     
-    if(isNaN(number)) return false;
+    if(isNaN(number)) return (0).toFixed(2);
+    
+    if(onlyformat) return (number).toFixed(2);
 
     return new Intl.NumberFormat('es-PE', 
     {
