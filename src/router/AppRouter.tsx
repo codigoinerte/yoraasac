b9aeaa42ed8f63e764f21afaf3c1a07461f38171
codigoinerte <fmartinez.bpe@gmail.com>
2025-01-29 19:07:20 -0500

    fix: implementacion de reseto de nota heladero en cambio de vista

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,19 +1,29 @@
 import React, { useEffect } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import { AuthRouter } from '../auth';
 import { PanelRoute } from '../panel';
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { useAuthStore } from '../hooks';
+import { useAuthStore, useNotaHeladeroStore } from '../hooks';
 
 export const AppRouter = () => {
 
   const { status, checkAuthToken } = useAuthStore();
-    
+  const { pathname } = useLocation();
+  const { setNullNotaHeladero } = useNotaHeladeroStore();
+
   useEffect(() => {
   
     checkAuthToken();
 
   }, []);
+
+  useEffect(() => {
+    if(!pathname.includes('nota-heladero')){
+      console.info('reseteo');
+      setNullNotaHeladero();
+    }
+  }, [pathname])
+  
 
   if ( status === 'checking' ) {
       return (
@@ -21,6 +31,7 @@ export const AppRouter = () => {
       )
   }
 
+  
   return (
     <Routes>
 
