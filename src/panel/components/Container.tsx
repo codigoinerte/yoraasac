326eaf7c12d6bd7222e87c98f74d939f6581b618
codigoinerte <fmartinez.bpe@gmail.com>
2026-02-02 08:28:35 -0500

    implementacion de tailwind merge

--- src/panel/components/Container.tsx
+++ src/panel/components/Container.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { twMerge } from 'tailwind-merge'
 import { ContainerInterface } from '../interfaces'
 
 export const Container = ({children, classContainer, className}:ContainerInterface) => {
   
   return (
     <>
-        <div className={`main inner-page  flex-1 bg-stone-50 ${classContainer} ${className}`}>
+        <div className={twMerge('main inner-page flex-1 bg-stone-50', classContainer, className)}>
             <div className="container">
                 {children}
             </div>
